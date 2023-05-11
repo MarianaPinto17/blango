8156b5596167160d9eb992b11b9dbc0014362270
Mariana Pinto <mipinto@ubiwhere.com>
2023-05-11 22:51:54 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from blog.models import Post
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from blog.forms import CommentForm
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -13,6 +15,8 @@ def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
+
+
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
