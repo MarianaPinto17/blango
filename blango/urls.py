2a0dbd75013e3551357196dede011e94619f2414
Mariana Pinto <mipinto@ubiwhere.com>
2023-05-18 04:03:54 +0000

    Finish django Allauth

--- blango/urls.py
+++ blango/urls.py
@@ -42,6 +42,6 @@ if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include(debug_toolbar.urls)),
         path("accounts/", include("django_registration.backends.activation.urls")),
-
+        path("accounts/", include("allauth.urls")),
     ]
 
