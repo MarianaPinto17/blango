3acccbd0919dd04629beab5428f60f26ad118d27
Mariana Pinto <mipinto@ubiwhere.com>
2023-05-20 23:28:31 +0000

    Finish DRF views

--- blango/urls.py
+++ blango/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
     path("accounts/register/",RegistrationView.as_view(form_class=BlangoRegistrationForm),name="django_registration_register",),
-    path("api/v1/", include("blog.api_urls")),
+    path("api/v1/", include("blog.api.urls"))
 ]
 
 if settings.DEBUG:
